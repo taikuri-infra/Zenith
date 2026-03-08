f2813538ed26aade46342a7730f5b379b7b2c62d
babakDoraniArab <babak.dorani@gmail.com>
2026-03-09 01:31:52 +0200

    fix(api): add sslmode=disable for migrations, log migration version
    golang-migrate requires explicit sslmode parameter. Also adds logging
    of current migration version to help diagnose migration issues.

--- services/api/internal/adapters/postgres/migrate.go
+++ services/api/internal/adapters/postgres/migrate.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
+	"log/slog"
+	"net/url"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
@@ -12,6 +14,15 @@ import (
 
 // RunMigrations applies all pending database migrations using the embedded FS.
 func RunMigrations(dsn string, migrations fs.FS) error {
+	// Ensure sslmode is set (golang-migrate requires explicit SSL config)
+	u, err := url.Parse(dsn)
+	if err == nil && u.Query().Get("sslmode") == "" {
+		q := u.Query()
+		q.Set("sslmode", "disable")
+		u.RawQuery = q.Encode()
+		dsn = u.String()
+	}
+
 	source, err := iofs.New(migrations, ".")
 	if err != nil {
 		return fmt.Errorf("open migration source: %w", err)
@@ -23,8 +34,16 @@ func RunMigrations(dsn string, migrations fs.FS) error {
 	}
 	defer m.Close()
 
-	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		return fmt.Errorf("run migrations: %w", err)
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			v, dirty, _ := m.Version()
+			slog.Info("migrations up to date", "version", v, "dirty", dirty)
+		} else {
+			return fmt.Errorf("run migrations: %w", err)
+		}
+	} else {
+		v, _, _ := m.Version()
+		slog.Info("migrations completed", "version", v)
 	}
 
 	return nil
